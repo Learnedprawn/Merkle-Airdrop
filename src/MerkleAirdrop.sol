2a2b2a6bf8435ab6c025f1820743a31ebce8ccb6
Learnedprawn <vineshparalkar@gmail.com>
2025-02-10 22:58:14 +0530

    completed merkleAirdrop

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,4 +1,46 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract MerkleAirdrop {}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
+
+    event Claim(address indexed account, uint256 amount);
+
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    mapping(address claimer => bool hasClaimed) public s_hasClaimed;
+
+    constructor(bytes32 _merkleRoot, IERC20 _airdropToken) {
+        i_merkleRoot = _merkleRoot;
+        i_airdropToken = _airdropToken;
+    }
+
+    function claim(address _account, uint256 _amount, bytes32[] calldata _merkleProof) external {
+        if (s_hasClaimed[_account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(_account, _amount))));
+        if (!MerkleProof.verify(_merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit Claim(_account, _amount);
+        s_hasClaimed[_account] = true;
+        i_airdropToken.safeTransfer(_account, _amount);
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
+}
