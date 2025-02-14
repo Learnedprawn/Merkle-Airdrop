0321ea314ce6649c3a8d54de085ee4c45bf0cf9d
Learnedprawn <vineshparalkar@gmail.com>
2025-02-14 12:27:36 +0530

    makefile

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -7,7 +7,7 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract MerkleAirdrop is EIP712{
+contract MerkleAirdrop is EIP712 {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop__InvalidProof();
@@ -25,6 +25,7 @@ contract MerkleAirdrop is EIP712{
         address account;
         uint256 amount;
     }
+
     bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account,uint256 amount)");
 
     constructor(bytes32 _merkleRoot, IERC20 _airdropToken) EIP712("MerkleAirdrop", "1") {
@@ -32,8 +33,9 @@ contract MerkleAirdrop is EIP712{
         i_airdropToken = _airdropToken;
     }
 
-    function claim(address _account, uint256 _amount, bytes32[] calldata _merkleProof, uint8 _v, bytes32 _r, bytes32 _s) external {
-
+    function claim(address _account, uint256 _amount, bytes32[] calldata _merkleProof, uint8 _v, bytes32 _r, bytes32 _s)
+        external
+    {
         if (s_hasClaimed[_account]) {
             revert MerkleAirdrop__AlreadyClaimed();
         }
@@ -51,11 +53,17 @@ contract MerkleAirdrop is EIP712{
     }
 
     function getMessageDigest(address _account, uint256 _amount) public view returns (bytes32) {
-        return _hashTypedDataV4(keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: _account, amount: _amount}))));
+        return _hashTypedDataV4(
+            keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: _account, amount: _amount})))
+        );
     }
 
-    function _isValidSignature(address _account, bytes32 _digest, uint8 _v, bytes32 _r, bytes32 _s) internal pure returns (bool) {
-        (address actualSigner, ,) = ECDSA.tryRecover(_digest, _v, _r, _s);
+    function _isValidSignature(address _account, bytes32 _digest, uint8 _v, bytes32 _r, bytes32 _s)
+        internal
+        pure
+        returns (bool)
+    {
+        (address actualSigner,,) = ECDSA.tryRecover(_digest, _v, _r, _s);
         return actualSigner == _account;
     }
 
